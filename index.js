40dba4f6a86ab4610bee2475e9ae3c8f42491e11
Paulo Henrique Epifanio de Oliveira <pfanio@gmail.com>
2021-10-30 17:59:25 -0300

    Endpoint inicial

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get("/home", (req, res)=>{
     res.json({mensagem: 'Página Home', codigo: 200} )
 })
 
+app.get("/faleconosco", (req, res)=>{
+    res.json({mensagem: 'Entre em contato com a gente!', codigo: 200} )
+})
+
+app.get("/vagas", (req, res)=>{
+    res.json({mensagem: 'No momento não temos vagas!', codigo: 200} )
+})
+
 app.listen(process.env.PORT || 8020) 
