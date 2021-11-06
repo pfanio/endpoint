8dea5c9fbc52baf32c5a9a057e7b21da8df7f679
Paulo Henrique Epifanio de Oliveira <pfanio@gmail.com>
2021-11-06 15:31:30 -0300

    Atualização de msg

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 const Express = require('express')
 const cors = require('cors')
 const app = Express()
-app.use(express.json());
+app.use(Express.json());
 
 const logins = [
-    {username: "paulo@ph.com", password:"1234"},
-    {username: "jose@bol.com", password:"4768"}
+    { username: "paulo@ph.com", password: "1234" },
+    { username: "jose@bol.com", password: "4768" }
 ]
 
 const options = {
     origin: "*"
-  }
-  app.use(cors(options));
+}
+app.use(cors(options));
 
-app.get("/home", (req, res)=>{
-    res.json({mensagem: 'Página Home', codigo: 200} )
+app.get("/home", (req, res) => {
+    res.json({ mensagem: 'Página Home', codigo: 200 })
 })
 
-app.get("/login", (req, res)=>{
+app.get("/login", (req, res) => {
 
     //username = req.body.username;
     //password = req.body.password;
 
-    res.json(req.body);
-    
+    res.json(req.body)
+
     //res.json({token: null} )
 
     //res.json({token: "auefiuefah837602"} )
 })
 
-app.get("/faleconosco", (req, res)=>{
-    res.json({mensagem: 'Entre em contato com a gente!', codigo: 200} )
+app.get("/faleconosco", (req, res) => {
+    res.json({ mensagem: 'Entre em contato com a gente!', codigo: 200 })
 })
 
-app.get("/vagas", (req, res)=>{
-    res.json({mensagem: 'No momento não temos vagas para desenvolvedores!', codigo: 200} )
+app.get("/vagas", (req, res) => {
+    res.json({ mensagem: 'No momento não temos vagas para desenvolvedores!', codigo: 200 })
 })
 
-app.listen(process.env.PORT || 8020) 
+app.listen(process.env.PORT || 8020)
